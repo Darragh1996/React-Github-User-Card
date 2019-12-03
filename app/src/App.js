72ab3a9998b4440d281588a4c3be771a22670c48
Darragh Ferry <darraghferry@hotmail.com>
2019-12-03 15:33:10 +0000

    changed App from a function to a class and added basic form component

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
 import List from "./components/List";
+import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Github API Project</h1>
-        <List />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Github API Project</h1>
+          <Form />
+          <List />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
